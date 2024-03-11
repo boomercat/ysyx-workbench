620454430097506f9ffefb5aea637e326f6d7b5d
baijiahao123 <782537644@qq.com>
2024-03-11 10:05:44 +0800

    daily commit ,save the work

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vtop.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h> 
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+static void single_cycle() {
+  dut.a = rand() & 1; dut.eval();
+  dut.b = rand() & 1; dut.eval();
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+
+  while(1) 
+  {
+    nvboard_update();
+    sleep(1);
+    single_cycle();
+  }
 }
--- npc/vsrc/a_b.v
+++ npc/vsrc/a_b.v
@@ -1,7 +1,7 @@
-module top(
+module a_b(
 	input a,
 	input b,
 	output f
 );
 	assign f = a ^ b;
-	endmodule
+endmodule
