17f7b45e80b5383156d94b8c083418cd1c7c24c6
tracer-ysyx <tracer@ysyx.org>
2024-03-13 12:44:55 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:44:55 up  1:19,  1 user,  load average: 0.11, 0.22, 0.20

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -35,7 +35,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
-	~/ysyx-workbench/nemu/build/risv32-nemu-interpreter $(ARGS)  
+	$(NEMU_HOME)/build/risv32-nemu-interpreter $(ARGS)  
 
 
 
