0249b62d7180315fc9300b4be98e1435b22b004f
tracer-ysyx <tracer@ysyx.org>
2024-03-13 21:57:46 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:57:46 up  6:09,  1 user,  load average: 0.81, 0.60, 0.38

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -63,9 +63,8 @@ static int cmd_info(char *args){
 }
 static int cmd_x(char *args){
   int num_a = atoi(strtok(args," "));
-  char *test = strtok(NULL," ");
-  paddr_t addr = strtoul(test,NULL, 0); // 32位
-  printf("%c  %x \n %d this is a test",*test,addr,num_a);
+  paddr_t addr = strtoul(strtok(NULL," "),NULL, 0); // 32位
+  //printf(" %x \n %d this is a test",addr,num_a);
   for (int i = 0; i < num_a; i++)
   {
     printf("%x:%08x\n",addr,paddr_read(addr,4));
