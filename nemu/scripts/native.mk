2d6a07cef3e56ab3278da977e9c273e3845a8ef1
baijiahao123 <782537644@qq.com>
2024-03-13 22:04:29 +0800

    PA_1 finished

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,20 +24,19 @@ $(BINARY):: compile_git
 
 # Some convenient rules
 
-override ARGS ?= -l $(BUILD_DIR)/nemu-log.txt
+override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?= 
-NEMU_EXEC := $(BINARY) $(ARGS) $(IMG) 
+NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
-	$(NEMU_EXEC) || echo "nemu quit" 
-
-
+	echo $(BINARY)
+	$(NEMU_EXEC)
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
