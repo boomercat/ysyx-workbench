09135dbe0cb4d323b85a29cc6cf1aa55c4cd9379
tracer-ysyx <tracer@ysyx.org>
2024-03-19 18:57:10 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:10 up 49 min,  1 user,  load average: 0.47, 0.49, 0.51

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -44,7 +44,7 @@ static struct rule {
   {"\\/", TK_DIV},         //我也不会英文
   {"\\*", TK_MUL},         //mutiple
   {"\\-", TK_SUB},          //minus
-  {"\\d",TK_NUM},        //integrity
+  {"\\w",TK_NUM},        //integrity
 };
 
 #define NR_REGEX ARRLEN(rules)
