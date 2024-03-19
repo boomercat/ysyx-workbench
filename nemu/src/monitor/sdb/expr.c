b21a0c68b084d450884b8d32bc496316351f35cb
tracer-ysyx <tracer@ysyx.org>
2024-03-19 20:53:59 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:59 up  2:46,  1 user,  load average: 0.64, 0.57, 0.55

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -120,6 +120,7 @@ static bool make_token(char *e) {
         break;
       }
     }
+    e[strlen(e)-1] = '\0';
     if ((i == NR_REGEX )&&(position != strlen(e) - 1)) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
