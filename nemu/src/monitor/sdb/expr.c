eed40ac02707bd2b647232cf8accabc3a43f722f
tracer-ysyx <tracer@ysyx.org>
2024-03-19 19:02:59 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:02:59 up 55 min,  1 user,  load average: 0.65, 0.59, 0.54

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -110,13 +110,13 @@ static bool make_token(char *e) {
           case TK_MUL:
           case TK_SUB:
           case '+':
-                      tokens[nr_token].type = rules[i].token_type;break;
+                      tokens[nr_token].type = rules[i].token_type;
           case TK_NUM:
                       if (substr_len <= 31)  substr_len = 31;
                       assert(substr_len<32);
                       strncpy(tokens[nr_token].str, substr_start, substr_len);     
                       nr_token++;
-                      break;
+            
           default: printf("Unprocess str \n");
         }
       }
