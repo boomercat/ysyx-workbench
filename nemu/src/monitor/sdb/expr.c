207d7d55a478af60cedd3f368d29aa4b83fb1296
tracer-ysyx <tracer@ysyx.org>
2024-03-20 19:24:08 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:08 up  8:43,  1 user,  load average: 0.38, 0.48, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -165,7 +165,7 @@ static bool check_parentheses(int p, int q) {
 }
 
 //计算p和q之间的主运算符函数
-static int main_operate(int p,int q){
+int main_operate(int p,int q){
   int locate = 0;
   printf("test the main_operate function");
   int tmp = p;
