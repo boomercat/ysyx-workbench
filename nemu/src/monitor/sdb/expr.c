0f0e954de2191d46e29809aa335c538904ff76a4
tracer-ysyx <tracer@ysyx.org>
2024-03-23 20:47:46 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:46 up  9:04,  1 user,  load average: 0.99, 0.51, 0.36

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -241,7 +241,9 @@ word_t eval(int p,int q){
       case TK_EQ  : return (val1 == val2);
       case TK_NEQ : return (val1 != val2);
       case TK_AND : return (val1 && val2);
-      default: assert(0);
+      default:  
+                assert(0);
+
       }
     }
   return 0;
