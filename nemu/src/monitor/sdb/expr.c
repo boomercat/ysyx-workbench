4e713b26b2db39a41c5206332f0a76ead24222f8
baijiahao123 <782537644@qq.com>
2024-04-04 17:28:28 +0800

    daily commit

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -33,7 +33,7 @@ static struct rule {
 } rules[] = {
   {"\\s", TK_NOTYPE},    // spaces
   {"(0x|0X)\\w+",TK_HEX},    //十六进制;
-  {"\\$(\\$0|ra|[sgt]p|t[0-6]|a[0-7]|pc|s([0-9]|1[0-1]))",TK_REG}, //reg 
+  {"\\$(\\$0|ra|[sgt]p|t[0-6]|a[0-7]|s([0-9]|1[0-1]))",TK_REG}, //reg 
   {"\\+", TK_PLUS},         // plus
   {"==", TK_EQ},        // equal
   {"!=", TK_NEQ},       // not equal
