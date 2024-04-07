e1beef72539ba4d5626e22de49dabaabd8532ad9
tracer-ysyx <tracer@ysyx.org>
2024-04-07 19:24:06 +0800

    > sim RTL 2371287 白白白 Linux bai 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:06 up  4:11,  1 user,  load average: 1.42, 1.10, 0.96

--- npc/top_trace_main.cpp
+++ npc/top_trace_main.cpp
@@ -4,57 +4,46 @@
 #include "Vtop.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+
 static Vtop dut;
-u_int32_t *init_mem(size_t size);
-u_int32_t guest_to_host(u_int32_t addr);
-u_int32_t pmem_read(u_int32_t *memory,u_int32_t vaddr);
 
-void single_cycle(){
-	dut.clk=0;dut.eval();
-	dut.clk=1;dut.eval();
+// 假设这些函数在其他地方已经定义
+uint32_t *init_mem(size_t size);
+uint32_t guest_to_host(uint32_t addr);
+uint32_t pmem_read(uint32_t *memory, uint32_t vaddr);
 
+void single_cycle() {
+    dut.clk = 0; dut.eval();
+    dut.clk = 1; dut.eval();
 }
 
-static void reset(int n){
-	dut.rst = 1;
-	while(n-- > 0) single_cycle();
-	dut.rst = 0;
+static void reset(int n) {
+    dut.rst = 1;
+    while(n-- > 0) single_cycle();
+    dut.rst = 0;
 }
 
-int main( ){
-	__uint32_t *memory;
-	memory = init_mem(3);
-	//初始化一个VerilatedCOntext对象，把地址复制给contextp。为verilator仿真做准备
-	VerilatedContext* contextp = new VerilatedContext;
-	//contextp->commandArgs(argc, argv);
-	contextp->traceEverOn(true);//追踪波形
+int main() {
+    uint32_t *memory;
+    memory = init_mem(3);
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->traceEverOn(true);
 
-	//const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};  
-	//初始化一个 VerilatedVcdc的类，把地址赋值给指针tfp。
     VerilatedVcdC* tfp = new VerilatedVcdC;
-	dut.trace(tfp,5); //将top模块的模型信息输出到tfp指向的vcd对象
-	tfp->open("obj_dir/wave.vcd");//打开波形文件
-	/*
-	while(!contextp->gotFinish()){
-    //while (contextp->time() < sim_time && !contextp->gotFinish()) {
+    dut.trace(tfp, 5);
+    tfp->open("obj_dir/wave.vcd");
 
+    reset(10);
+    // 设置PC的值，注意这里改为了C++中的数值表示
+    dut.pc = 0x80000000;
+    for(int i = 0; i < 4; i++) {
+        dut.instruction = pmem_read(memory, dut.pc);
+        single_cycle();
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+    }
 
-		top->inst = pmem_read(top->pc);
-	    tfp->dump(contextp->time());
-		contextp->timeInc(1); 
-		top->eval();
-		tfp->dump(contextp->time());
-	}*/
-	reset(10);
-	dut.pc = 0b10000000000000000000000000000000
-	for(int i = 0;i < 4;i++){
-		dut.instruction = pmem_read(memory,dut.pc);
-		single_cycle();
-		tfp->dump(contextp->time());
-		contextp->timeInc(1);
-	}
-	tfp->close();
-	delete contextp;
-	return 0;
+    tfp->close();
+    delete contextp;
+    return 0;
 }
-
