0f982e83f84de31991c20000c8be33ecc57054b1
baijiahao123 <782537644@qq.com>
2024-04-07 19:47:37 +0800

    daily commit

---
+++ npc/vsrc/IDU.v
@@ -0,0 +1,19 @@
+module IDU(
+    input [31:0] instruction,
+    output reg [4:0] rs1,
+    output reg [4:0] rd,
+    output reg [31:0] imm,
+    output reg [6:0] opcode
+    //output reg [2:0] funct3
+    // 还可以根据需要输出更多的控制信号
+);
+
+assign  opcode = instruction[6:0];
+assign  rs1 = instruction[19:15];
+assign  rd = instruction[11:7];
+assign  imm = {{20{instruction[31]}},instruction[31:20]};  // 立即数的符号扩展
+//assign funct3 = instruction[14:12];
+    // 根据opcode设置其他控制信号
+
+
+endmodule
