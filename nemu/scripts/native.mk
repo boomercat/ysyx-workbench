99a233b6293ede80328a057ab517973ebd164ed6
tracer-ysyx <tracer@ysyx.org>
2024-04-10 15:42:48 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:48 up  5:43,  3 users,  load average: 0.30, 0.37, 0.42

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -24,7 +24,7 @@ $(BINARY):: compile_git
 
 # Some convenient rules
 
-override ARGS ?= -l $(BUILD_DIR)/nemu-log.txt  
+override ARGS ?= -l $(BUILD_DIR)/nemu-log.txt  -b
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
