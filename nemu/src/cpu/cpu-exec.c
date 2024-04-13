46601a381f9c11da4b10089a57f9d9db72e7395b
tracer-ysyx <tracer@ysyx.org>
2024-04-13 14:47:31 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:31 up  1:04,  1 user,  load average: 0.79, 0.76, 0.74

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -120,6 +120,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     //终止or中断
     case NEMU_END: case NEMU_ABORT:
+      display_inst();
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
