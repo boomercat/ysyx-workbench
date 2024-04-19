c6051eeee7a6f1aed112a87a83a6fb543bdfeb03
tracer-ysyx <tracer@ysyx.org>
2024-04-19 18:30:40 +0800

    > compile NEMU 2371287 白白白 Linux bai 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:30:40 up  8:34,  1 user,  load average: 0.64, 0.57, 0.35

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,10 +16,21 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
-
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+//把通用寄存器和PC与从DUT中读出寄存器值进行比较
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) { 
+  int reg_num = ARRLEN(cpu.gpr);
+  for (int i = 0; i < reg_num; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      return false;
+    }
+  }
+  if (ref_r->pc != cpu.pc) {
+    return false;
+  }
+  return true;
 }
+ 
+ 
 
 void isa_difftest_attach() {
 }
