931c033de221c1893e2f32c4b6b03bdb7ce4d2ab
baijiahao123 <782537644@qq.com>
2024-05-12 20:45:33 +0800

    daily save

--- npc/vsrc/RegisterFile.v
+++ npc/vsrc/RegisterFile.v
@@ -7,11 +7,21 @@ module RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   output [DATA_WIDTH-1:0] RData1 //output the add1's data 
 
 );
-  reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0]; //寄存器文件数组
+import "DPI-C" function void update_regs(int idx,input word_t data);
+integer j;
+reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0]; //寄存器文件数组
   //write operation
-  always @(posedge clk) begin
-    if ((wen) && waddr != 0) rf[waddr] <= wdata;
+always @(posedge clk) begin
+  if (wen && waddr != 0)  rf[waddr] <= wdata;
+      //update_regs(waddr, wdata); // 使用正确的变量名
+end
+
+
+always@(*) begin
+ for ( j = 0 ; j < 32;j++ ) 
+      update_regs(j,rf[j]);    
   end
+
 
   //read operation and 0 register
   assign RData1 = (Addr1==0) ? 0 : rf[Addr1];
