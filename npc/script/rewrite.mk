503fe39ba01cd43d42e8b02391bfe99c743e9d2f
baijiahao123 <782537644@qq.com>
2024-07-22 11:18:57 +0800

    OpenXiangShan/chisel-playground master initialized
    OpenXiangShan/chisel-playground 5d8816d81c648382511e6599121fae57738929ec Merge pull request #9 from dzwduan/master

--- npc/script/rewrite.mk
+++
@@ -1,6 +0,0 @@
-include Vtop.mk
-CPPFLAGS += $(shell llvm-config --cxxflags | tr ' ' '\n' | grep '^-I' | tr '\n' ' ')
-#CPPFLAGS += -DCONFIG_FTRACE=1
-#CPPFLAGS += -DCONFIG_TRACE=1
-LDFLAGS += $(shell llvm-config --ldflags)
-LIBS += $(shell llvm-config --libs) #please make sure you already have llvm-config or in your PATH
